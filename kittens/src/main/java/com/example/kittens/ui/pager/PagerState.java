2dc18fa1f78703d2130432bdba92898a5bfe9603
Simon Edström <simon.edstrom@screeninteraction.com>
2017-07-17 12:39:44 +0200

    Update kittens sample.

--- kittens/src/main/java/com/example/kittens/ui/pager/PagerState.java
+++ kittens/src/main/java/com/example/kittens/ui/pager/PagerState.java
@@ -10,36 +10,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import se.snylt.witch.annotations.BindTo;
+import se.snylt.witch.annotations.BindToView;
+import se.snylt.witch.annotations.BindToViewPager;
 import se.snylt.witch.annotations.Binds;
 import se.snylt.witch.viewbinder.bindaction.Binder;
-import se.snylt.witch.viewbinder.bindaction.SyncOnBind;
 
 import static com.example.kittens.utils.BinderUtils.toast;
 
 class PagerState {
 
+    // Views are bound in same order as declared
+    // kittens needs to be bound before startPosition
+    @BindToViewPager(id = R.id.view_pager, adapter = KittenPagerAdapter.class, set = "kittens")
     List<Kitten> kittens = new ArrayList<>();
 
+    @BindToView(id = R.id.view_pager, view = ViewPager.class, set = "currentItem")
     int startPosition = 0;
-
-    @BindTo(R.id.view_pager)
-    final PagerState state = this;
-
-    // This is a bit of a hack. Binding has to happen in a certain order, which is not yet supported.
-    @Binds
-    Binder<ViewPager, PagerState> bindsState = Binder.create(new SyncOnBind<ViewPager, PagerState>() {
-        @Override
-        public void onBind(ViewPager viewPager, PagerState pagerState) {
-            ((KittenPagerAdapter)viewPager.getAdapter()).setKittens(pagerState.kittens);
-            viewPager.setCurrentItem(startPosition, false);
-        }
-    });
 
     @BindTo(R.id.view_pager)
     String instructions = "Swipe to see more kittens :)";
 
     @Binds
     Binder<View, String> bindsInstructions = Binder.create(toast());
-
 
 }
